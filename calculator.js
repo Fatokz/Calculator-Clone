5e147e6dce9b5d66f3ae801521cfacc8e2f95294
FATOKUN EMMANUEL <fatokunemmanuel16@gmail.com>
2024-03-14 07:39:55 +0100

    editing of script

--- calculator.js
+++ calculator.js
@@ -1,20 +1,29 @@
-let show = document.getElementById("out")
+let out = document.getElementById("output");
+let show = document.getElementById("out");
 // show.innerHTML = 0;
 
 // function any(e) {
 //     show.innerHTML = ''
 // }
 //Event is 
+// function calculate() {
+//     if (out.innerHTML.includes("+")) {
+//         let numbers = value.split("+")
+//         out.innerHTML = Number(numbers[0]) + Number(numbers[1])
+//     }
+// }
+
 function any(e) {
     // show.innerHTML = ''
     console.log(e.target.value);
-    // const value = e.target.value;
+    out.innerHTML += e.target.value;
     show.innerHTML += e.target.value;
 }
 
 function calculate(e) {
     // show.innerHTML = ''
     console.log(e.target.value);
+    out.innerHTML += e.target.value;
     show.innerHTML += e.target.value;
 }
 
@@ -22,31 +31,38 @@ function equalTo() {
     // show.innerHTML = ''
     let value = show.innerHTML;
     console.log(value);
+    
 
-    if (show.innerHTML.includes("+")) {
+    if (show.innerHTML.includes("+")){
         let numbers = value.split("+")
+        // show.innerHTML = (Math.floor(Number(numbers[0]) + Number(numbers[1])*10))
         show.innerHTML = Number(numbers[0]) + Number(numbers[1])
+        console.log(show.innerHTML);
     }
 
     else if (show.innerHTML.includes("-")) {
         let numbers = value.split("-")
         show.innerHTML = Number(numbers[0]) - Number(numbers[1])
+        console.log(show.innerHTML);
     }
     
 
     else if (show.innerHTML.includes("*")) {
         let numbers = value.split("*")
         show.innerHTML = Number(numbers[0]) * Number(numbers[1])
+        console.log(show.innerHTML);
     } 
     
     else if (show.innerHTML.includes("^")) {
         let numbers = value.split("^")
         show.innerHTML = Number(numbers[0]) ** Number(numbers[1])
+        console.log(show.innerHTML);
     }
 
     else {
         let numbers = value.split("÷")
         show.innerHTML = Number(numbers[0]) / Number(numbers[1])
+        console.log(show.innerHTML);
     }
 
 
@@ -57,6 +73,7 @@ function equalTo() {
 function cls() {
     let text = show.innerHTML
     console.log(text);
+    out.innerHTML = text.slice(0, show.innerHTML.length - 1)
     show.innerHTML = text.slice(0, show.innerHTML.length - 1)
 }
 
@@ -64,6 +81,7 @@ function clsAll() {
     let text = show.innerHTML
     console.log(text);
     show.innerHTML = text.slice(0, show.innerHTML.length - show.innerHTML.length)
+    out.innerHTML = text.slice(0, show.innerHTML.length - show.innerHTML.length)
     // show.innerHTML = ''
 }
 
